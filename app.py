752a6e9f45e47a41844bae37bc96b5f47f65033f
Katerina <katkot19@gmail.com>
2026-01-29 14:30:10 +0200

    Αλλαγές app.py, index.html, προσθήκη requirements.txt

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from flask import Flask, render_template
 import sqlite3
+import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "cafes.db")
+
+
 def get_cafes():
-    conn = sqlite3.connect("cafes.db")
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute("SELECT name, location, has_wifi, has_sockets FROM cafes")
+
+    cursor.execute("""
+        SELECT name, location, has_wifi, has_sockets
+        FROM cafe
+    """)
+
     cafes = cursor.fetchall()
     conn.close()
     return cafes
+
 
 @app.route("/")
 def index():
     cafes = get_cafes()
     return render_template("index.html", cafes=cafes)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
